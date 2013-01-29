a5c0d86b64b736fd529f0e792599801b49af43a7
Shaun Dern <shaun@substantial.com>
2013-01-29 15:23:07 -0800

    update task_builder. Remove #all until later

--- lib/sous-chef/task_builder.rb
+++ lib/sous-chef/task_builder.rb
@@ -7,27 +7,19 @@ class SousChef::TaskBuilder
   end
 
   def build_tasks
-    @nodes.each do |name, collection|
+    build_namespace(@nodes)
+  end
+
+  def build_namespace(collection)
+    collection.each do |name, collection|
       if node?(collection)
         build_node_task(collection)
       else
-        SousChef::TaskBuilder.new(collection).build_tasks
+        namespace name do
+          build_namespace(collection)
+        end
       end
     end
-    #   namespace :all do
-    #     desc "Run knife solo prepare for all #{environment_name} nodes"
-    #     multitask :prepare => build_task_list(environment_name, env.nodes.keys, "prepare")
-
-    #     desc "Run knife solo cook for all #{environment_name} nodes"
-    #     multitask :cook => build_task_list(environment_name, env.nodes.keys, "cook")
-
-    #     desc "Run knife solo bootstrap for all #{environment_name} nodes"
-    #     multitask :bootstrap => build_task_list(environment_name, env.nodes.keys, "bootstrap")
-    #   end
-
-    #   env.nodes.each do |node_name, node|
-    #     build_node_task(node)
-    #   end
   end
 end
 
