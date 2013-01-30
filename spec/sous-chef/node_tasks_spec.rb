d093b4cb673b33ebac9ab00f6795af6ff11046af
Shaun Dern <shaun@substantial.com>
2013-01-29 16:28:23 -0800

    cleanup node_tasks_spec

--- spec/sous-chef/node_tasks_spec.rb
+++ spec/sous-chef/node_tasks_spec.rb
@@ -1,22 +1,17 @@
 require 'spec_helper'
 
 describe SousChef::NodeTasks do
-
   before do
     SousChef::NodeTasks.stub(:run)
   end
 
   describe "#run_knife" do
-    let(:node) { double('node') }
-
-    before do
+    it "should run knife solo command for given node" do
+      node = double('node')
       node.stub(name: 'foo')
       node.stub(hostname: 'node_hostname')
       node.stub(ssh_config_path: 'ssh_config_path')
       node.stub(config: 'knife_solo_node_config')
-    end
-
-    it "should run knife solo command for given node" do
       SousChef::NodeTasks.should_receive(:run).with("knife solo some_command -F ssh_config_path node_hostname -N foo knife_solo_node_config")
       SousChef::NodeTasks.run_knife('some_command', node)
     end
