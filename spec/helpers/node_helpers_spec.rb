0215330cddcd3f75f9b17551182f8031a2c88ead
Shaun Dern <shaun@substantial.com>
2013-01-29 16:26:27 -0800

    cleanup node_helper_spec

--- spec/helpers/node_helpers_spec.rb
+++ spec/helpers/node_helpers_spec.rb
@@ -3,42 +3,24 @@ require 'spec_helper'
 describe SousChef::NodeHelpers do
 
   describe "#node?" do
-    let(:settings_hash) { {} }
-
-    subject { SousChef::NodeHelpers.node?(settings_hash) }
-
-    context "with hash containing ssh_config" do
-      before { settings_hash['ssh_config'] = true }
-      it "should consider hashes with ssh_config key as a node" do
-        subject.should be_true
-      end
+    it "should consider hashes with ssh_config key as a node" do
+      SousChef::NodeHelpers.node?(SousChef::Node.new({'ssh_config' => true}, {})).should be_true
     end
 
-    context "with hash containing node_config" do
-      before { settings_hash['node_config'] = true }
-      it "should consider hashes with node_config key as a node" do
-        subject.should be_true
-      end
+    it "should consider hashes with node_config key as a node" do
+      SousChef::NodeHelpers.node?(SousChef::Node.new({'node_config' => true}, {})).should be_true
     end
 
-    context "with actual node" do
-      subject { SousChef::NodeHelpers.node?(SousChef::Node.new('some node', {})) }
-      it "should consider Node objects as a node" do
-        subject.should be_true
-      end
+    it "should consider Node objects as a node" do
+      SousChef::NodeHelpers.node?(SousChef::Node.new('some node', {})).should be_true
     end
 
-    context "with empty hash" do
-      it "shouldn't consider empty hashes as a node" do
-        subject.should be_false
-      end
+    it "shouldn't consider empty hashes as a node" do
+      SousChef::NodeHelpers.node?({}).should be_false
     end
 
-    context "with collection" do
-      subject { SousChef::NodeHelpers.node?(SousChef::Collection.new('some collection')) }
-      it "shouldn't consider Collection objects as a node" do
-        subject.should be_false
-      end
+    it "shouldn't consider Collection objects as a node" do
+      SousChef::NodeHelpers.node?(SousChef::Collection.new('some collection')).should be_false
     end
   end
 end
