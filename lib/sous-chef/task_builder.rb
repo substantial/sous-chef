9c0ae976d03ed5b5ca6f4c4d8c72b2b599d9e34f
Shaun Dern <shaun@substantial.com>
2013-01-29 16:22:08 -0800

    add #add multitask to namespaces

--- lib/sous-chef/task_builder.rb
+++ lib/sous-chef/task_builder.rb
@@ -8,6 +8,19 @@ class SousChef::TaskBuilder
 
   def build_tasks
     build_namespace(@nodes)
+    build_batch_tasks(@nodes)
+  end
+
+  def build_batch_tasks(collection)
+    collection.each do |name, collection|
+      next if node?(collection)
+      namespace name do |ns|
+        namespace :all do
+          batch_tasks(name, ns.tasks)
+        end
+        build_batch_tasks(collection)
+      end
+    end
   end
 
   def build_namespace(collection)
@@ -20,6 +33,23 @@ class SousChef::TaskBuilder
         end
       end
     end
+  end
+
+  private
+
+  def batch_tasks(name, tasks)
+    desc "Run knife solo prepare for all #{name} nodes"
+    multitask :prepare => filter_tasks(tasks, 'prepare')
+
+    desc "Run knife solo cook for all #{name} nodes"
+    multitask :cook => filter_tasks(tasks, 'cook')
+
+    desc "Run knife solo bootstrap for all #{name} nodes"
+    multitask :bootstrap => filter_tasks(tasks, 'bootstrap')
+  end
+
+  def filter_tasks(tasks, desired_task)
+    tasks.map(&:name).keep_if { |task_name| task_name.include? desired_task }
   end
 end
 
